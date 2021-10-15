1ba98687602b3053d7737dbc50bbb1bc91db16f1
GIT <support@rcelectronic.co.id>
2021-10-16 00:36:13 +0700

    add sidebar

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app :style="{ background: $vuetify.theme.themes.dark.background }">
+    <SideBar />
+    <SideBarRight />
+  </v-app>
+</template>
+
+<script>
+// @ is an alias to /src
+import SideBar from "@/components/SideBar.vue";
+import SideBarRight from "@/components/SideBarRight.vue";
+
+export default {
+  name: "Home",
+  components: {
+    SideBar,
+    SideBarRight,
+  },
+};
+</script>
